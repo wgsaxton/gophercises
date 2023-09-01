4709d78d71b1ebfb06579b20cd16ae11d31f74a4
Garrett Saxton <garrett.saxton@salesforce.com>
2023-09-01 08:50:44 -0600

    CYOA after parsing paths

---
+++ cyoa/cmd/cyoaweb/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/wsaxton/gophercises/cyoa"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
+	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s.\n", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
