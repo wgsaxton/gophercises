dc8e744e26ce9ef655c0085f54f4de91f8b5f6b8
Garrett Saxton <garrett.saxton@salesforce.com>
2023-09-11 22:43:04 -0600

    Done with link parser exercise

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/wsaxton/gophercises
 
 go 1.20
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	golang.org/x/net v0.9.0
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ link_parser/examples/ex1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	link "github.com/wsaxton/gophercises/link_parser"
+)
+
+var exampleHtml = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">
+    A link to another page
+    <span> some span </span>
+  </a>
+  <a href="/page-two">A link to second page</a>
+</body>
+</html>`
+
+func main() {
+	r := strings.NewReader(exampleHtml)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
